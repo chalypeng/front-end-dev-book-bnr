ba74a7ebb988f5d1d862f30c45b763557e88930e
chalypeng <chalypeng@gmail.com>
2017-02-23 15:35:56 +0800

    finished Chapter 7,and the OTTERGRAM Project done!

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -4,6 +4,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var DETAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -38,6 +42,7 @@ function addThumbClickHander(thumb) {
     thumb.addEventListener('click', function (event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
     });
 }
 
@@ -48,11 +53,36 @@ function getThumbnailsArray() {
     return thumbnailArray;
 }
 
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+function showDetails() {
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    },50);
+}
+function addKeyPresshandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+    });
+}
+
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHander);
     // console.log('init finished!');
+    addKeyPresshandler();
 }
 
 // execute initializeEvents
